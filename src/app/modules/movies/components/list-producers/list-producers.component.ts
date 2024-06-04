ce595da8fd173c031e938d3d4dd3fd95fc74d604
Matheus Souto <frameworktecnologia37@localiza.com>
2024-06-03 23:18:19 -0300

    🔵 other: Removendo mock da lista de produtores

--- src/app/modules/movies/components/list-producers/list-producers.component.ts
+++ src/app/modules/movies/components/list-producers/list-producers.component.ts
@@ -1,20 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
-export interface PeriodicElement {
-  producer: string;
-  interval: string;
-  previous: string;
-  following: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {producer: '1986', interval: '2', previous: 'sdsds', following: 'asdasd'},
-  {producer: '1986', interval: '2', previous: 'sdsds', following: 'asdasd'},
-  {producer: '1986', interval: '2', previous: 'sdsds', following: 'asdasd'},
-  {producer: '1986', interval: '2', previous: 'sdsds', following: 'asdasd'}
-];
-
 @Component({
   selector: 'app-list-producers',
   standalone: true,
